065b06bd275531097c0ae30e88eb915b082fc2a1
Michael Chan <mijoch@gmail.com>
2016-11-23 20:53:38 -0800

    feat: add CodeMirror

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
 import React, { Component } from "react";
 import postcss from "postcss"
 import cssnext from "postcss-cssnext"
+import CodeMirror from "react-codemirror"
+
+require('codemirror/lib/codemirror.css')
+require('codemirror/mode/css/css');
 
 const processor = (css4, callback) =>
   postcss([cssnext])
     .process(css4)
     .then(result => callback(result.css))
 
+const starterCSS = `
+:root { --pink: #f3c }
+
+.pink { color: var(--pink) }
+`
+
 class App extends Component {
   state = {
+    input: starterCSS,
     output: ""
   }
 
-  handleChange = (e) =>
-    processor(e.target.value, css3 =>
-      this.setState({output: css3})
+  updateWithCSS =(css4) =>
+    processor(css4, css3 =>
+      this.setState({
+        input: css4,
+        output: css3
+      })
     )
+
+  componentDidMount() {
+    this.updateWithCSS(this.state.input)
+  }
+
+  handleChange = (css4) => this.updateWithCSS(css4)
 
   render() {
     return (
-      <div>
-        <textarea onChange={this.handleChange} />
+      <div style={{ display: "flex" }}>
+        <div style={{ flex: 1 }}>
+          <h4>CSS4</h4>
+          <CodeMirror
+            onChange={this.handleChange}
+            options={{mode: "css"}}
+            value={this.state.input}
+          />
+        </div>
 
-        <pre>
-          <code>
-            {this.state.output}
-          </code>
-        </pre>
+        <div style={{ flex: 1 }}>
+          <h4>CSS3</h4>
+          <CodeMirror
+            value={this.state.output}
+            options={{
+              mode: "css",
+              readOnly: true,
+            }}
+          />
+      </div>
       </div>
     )
   }
